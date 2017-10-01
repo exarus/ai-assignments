b48c132ae9b01c4a41561bc14e8638372002feb4
Ruslan Gunawardana <mr.exarus@gmail.com>
2017-10-02 01:53:48 +0300

    drag&drop polyfill for all mobile devices added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
-<template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
-</template>
 
-<script>
-export default {
-  name: 'app'
-}
-</script>
+@import '~mobile-drag-drop/default.css'
+  @import '~element-ui/lib/theme-default/index.css'
 
-<style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-</style>
