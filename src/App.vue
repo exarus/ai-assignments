37861ca1bc24d0a9e4d14b94678ae1c2c07307e6
Ruslan Gunawardana <mr.exarus@gmail.com>
2017-10-02 17:52:32 +0300

    lodash-es -> ramda

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
 
-@import '~mobile-drag-drop/default.css'
-  @import '~element-ui/lib/theme-default/index.css'
+<script>
+import { polyfill as dragAndDropPolyfill } from 'mobile-drag-drop'
+
+dragAndDropPolyfill()
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import '~mobile-drag-drop/default.css';
+@import '~element-ui/lib/theme-default/index.css';
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+</style>
