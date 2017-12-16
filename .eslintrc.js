16794366e598fbd64e0b2609cb37d0a14ab72dfa
Ruslan Gunawardana <mr.exarus@gmail.com>
2017-12-16 07:08:25 +0200

    Dependencies updated. Fresh vue template version merged.

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,6 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    // allow paren-less arrow functions
-    'arrow-parens': ['error', 'as-needed', { 'requireForBlockBody': true }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'compat/compat': 'error'
